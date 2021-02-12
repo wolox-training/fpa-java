0cc00f091c30d4d39cbe15e46f6af41504b23dd1
franciscopacheco <francisco.pacheco@wolox.co>
2021-02-12 12:57:29 -0500

    add optional in return findByAuthor and change JpaRepository to Repository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,10 +1,11 @@
 package wolox.training.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends Repository<Book, Long> {
 
-	Book findByAuthor(String author);
+	Optional<Book> findByAuthor(String author);
 
 }
