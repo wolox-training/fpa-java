796efc56788f0d0bae4859a93956669d24ef8876
franciscopacheco <francisco.pacheco@wolox.co>
2021-02-16 13:15:53 -0500

    change CrudRepository param Book toUsers

--- src/main/java/wolox/training/repositories/UsersReisitory.java
+++ src/main/java/wolox/training/repositories/UsersReisitory.java
@@ -1,11 +1,12 @@
 package wolox.training.repositories;
 
 import java.util.Optional;
+import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.Book;
 import wolox.training.models.Users;
 
-public interface UsersReisitory extends CrudRepository<Book, Long> {
+public interface UsersReisitory extends CrudRepository<Users, Long> {
 
 	Optional<Users> findByName(String name);
 
