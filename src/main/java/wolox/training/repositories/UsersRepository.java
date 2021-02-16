77e1d20a3edc4c71daee5a4c5a11474912f07228
franciscopacheco <francisco.pacheco@wolox.co>
2021-02-16 14:06:40 -0500

    rename user repository

--- src/main/java/wolox/training/repositories/UsersRepository.java
+++ src/main/java/wolox/training/repositories/UsersRepository.java
@@ -1,12 +1,10 @@
 package wolox.training.repositories;
 
 import java.util.Optional;
-import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
-import wolox.training.models.Book;
 import wolox.training.models.Users;
 
-public interface UsersReisitory extends CrudRepository<Users, Long> {
+public interface UsersRepository extends CrudRepository<Users, Long> {
 
 	Optional<Users> findByName(String name);
 
