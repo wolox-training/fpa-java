bc41e359b048541b77bc30c6f76a7deebfd8a2e9
franciscopacheco <francisco.pacheco@wolox.co>
2021-02-19 23:29:51 -0500

    add dependency lombook and implementation lombook in model user

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -1,14 +1,13 @@
 package wolox.training.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.sun.istack.NotNull;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import javax.persistence.CascadeType;
@@ -19,13 +18,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 import wolox.training.exceptions.NotificationCode;
-import wolox.training.utils.ValidationPreconditionUtil;
 
 @Entity
 @Table(name = "users")
 @ApiModel(description = "Users from the OpenLibrary")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
 	@Id
@@ -33,12 +39,15 @@ public class User {
 	private Long id;
 
 	@ApiModelProperty("The User username")
+	@NotNull
 	@Column(nullable = false)
 	private String username;
 
+	@NotNull
 	@Column(nullable = false)
 	private String name;
 
+	@NotNull
 	@Column(nullable = false)
 	@JsonDeserialize(using = LocalDateDeserializer.class)
 	@JsonSerialize(using = LocalDateSerializer.class)
@@ -47,60 +56,6 @@ public class User {
 	@ManyToMany(cascade = CascadeType.PERSIST)
 	private List<Book> books = new LinkedList<>();
 
-	public User() {
-	}
-
-	public User(Long id, String username, String name, LocalDate birthdate, List<Book> books) {
-		this.id = id;
-		this.username = username;
-		this.name = name;
-		this.birthdate = birthdate;
-		this.books = books;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		ValidationPreconditionUtil.validateFieldCheckNotNull(id,"id");
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		ValidationPreconditionUtil.validateFieldCheckNotNull(username,"username");
-		this.username = username;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		ValidationPreconditionUtil.validateFieldCheckNotNull(name,"name");
-		this.name = name;
-	}
-
-	public LocalDate getBirthdate() {
-		return birthdate;
-	}
-
-	public void setBirthdate(LocalDate birthdate) {
-		ValidationPreconditionUtil.validateFieldCheckNotNull(birthdate,"birthdate");
-		this.birthdate = birthdate;
-	}
-
-	public List<Book> getBooks() {
-		return (List<Book>) Collections.unmodifiableList(books);
-	}
-
-	public void setBooks(List<Book> books) {
-		this.books = books;
-	}
 
 	public List<Book> addBook(Book bookCurrent) {
 		if (books.stream().filter(book -> book.getId().equals(bookCurrent.getId())).findFirst().isPresent()) {
