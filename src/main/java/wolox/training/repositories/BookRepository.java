727e85b1988644a8b0798786f78bb66b3d30c048
franciscopacheco <francisco.pacheco@wolox.co>
2021-02-25 17:57:28 -0500

    change import

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,10 +1,11 @@
 package wolox.training.repositories;
 
-import feign.Param;
+
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import wolox.training.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
