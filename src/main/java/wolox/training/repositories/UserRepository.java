5f2621b57e56224ead66b7938c2424833387a70c
franciscopacheco <francisco.pacheco@wolox.co>
2021-02-26 08:55:21 -0500

    clean import and add query in user repository

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package wolox.training.repositories;
 
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -13,10 +12,8 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
 	Optional<User> findByUsername(String username);
 
-	@Query("SELECT u FROM User u WHERE (u.birthdate BETWEEN :startDate  AND :endDate) and (lower(u.name) like lower"
-			+ "(concat('%', :name,'%'))) ")
+	@Query("SELECT u FROM User u WHERE (u.birthdate BETWEEN :startDate  AND :endDate) and (lower(u.name) like lower(concat('%', :name,'%')))")
 	List<User> findByBirthdateBetweenAndNameContainingIgnoreCase(@Param("startDate") LocalDate startDate,
-			@Param("endDate") LocalDate endDate,
-			@Param("name")String name);
+			@Param("endDate") LocalDate endDate, @Param("name") String name);
 
 }
